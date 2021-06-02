5c7671d434d1c4a3ef869cb0c955d6e99a548422
andrewstech <45342431+andrewstech@users.noreply.github.com>
2021-06-02 16:04:23 +0100

    Update __main__.py

--- thealphavideo/__main__.py
+++ thealphavideo/__main__.py
@@ -17,7 +17,7 @@ from sentry_sdk.integrations.flask import FlaskIntegration
 
 
 def get_db_connection():
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect('/data/database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
