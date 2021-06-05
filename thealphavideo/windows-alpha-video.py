2f3eb1f919cba41928e40eb1dad30dc0330f720b
andrewstech <45342431+andrewstech@users.noreply.github.com>
2021-06-05 16:06:04 +0100

    Update windows-alpha-video.py

--- thealphavideo/windows-alpha-video.py
+++ thealphavideo/windows-alpha-video.py
@@ -67,10 +67,10 @@ ytdl_options = {
 
 ytdl = YoutubeDL(ytdl_options)
 app = Flask(__name__)
-ui = FlaskUI(app, port=5000, width=600, height=500)
 app.config["DEBUG"] = os.environ.get("FLASK_DEBUG", True)
 app.config["JSON_AS_ASCII"] = False
 app.config['SECRET_KEY'] = 'dev'
+app.config['PUBLIC']=os.environ.get("public", "False") == "True"
 app.config.from_mapping(
     BASE_URL="http://localhost:5000",
 )
@@ -88,112 +88,10 @@ def not_found_error(error):
     return render_template('405.html'), 405
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/playlist')
-def playlist():
-    conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
-    conn.close()
-    return render_template('playlist.html', posts=posts)
-
-
-@app.route('/create', methods=('GET', 'POST'))
-def create():
-    if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
-
-        if not title:
-            flash('Title is required!')
-        else:
-            conn = get_db_connection()
-            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
-            conn.commit()
-            conn.close()
-            return redirect(url_for('playlist'))
-
-    return render_template('create.html')
-
-
-@app.route('/<int:id>/delete', methods=('GET',))
-def delete(id):
-    post = get_post(id)
-    conn = get_db_connection()
-    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
-    conn.commit()
-    conn.close()
-    flash('"{}" was successfully deleted!'.format(post['title']))
-    return redirect(url_for('playlist'))
-
-
-@app.route('/<int:post_id>')
-def post(post_id):
-    post = get_post(post_id)
-    return render_template('post.html', post=post)
-
-
-@app.route('/<int:id>/edit', methods=('GET', 'POST'))
-def edit(id):
-    post = get_post(id)
-
-    if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
-
-        if not title:
-            flash('Title is required!')
-        else:
-            conn = get_db_connection()
-            conn.execute('UPDATE posts SET title = ?, content = ?'
-                         ' WHERE id = ?',
-                         (title, content, id))
-            conn.commit()
-            conn.close()
-            return redirect(url_for('playlist'))
-
-    return render_template('edit.html', post=post)
-
-
-@app.route('/progress')
-def progress():
-    def generate():
-        x = 0
-        while x <= 100:
-            yield "data:" + str(x) + "\n\n"
-            x = x + 10
-            time.sleep(0.5)
-
-    return Response(generate(), mimetype='text/event-stream')
-
-
-@app.route('/log')
-def progress_log():
-    def generate():
-        for line in Pygtail(LOG_FILE, every_n=1):
-            yield "data:" + str(line) + "\n\n"
-            time.sleep(0.5)
-
-    return Response(generate(), mimetype='text/event-stream')
-
-
-@app.route('/env')
-def show_env():
-    log.info("route =>'/env' - hit")
-    env = {}
-    for k, v in request.environ.items():
-        env[k] = str(v)
-    log.info("route =>'/env' [env]:\n%s" % env)
-    return env
-
-
-@app.route("/logstream", methods=["GET"])
-def logstream():
-    return render_template('logs.html')
+if app.config["PUBLIC"]:
+    import public
+else:
+    import pages 
 
 
 ask = Ask(app, '/api')
