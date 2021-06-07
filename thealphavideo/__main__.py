f5230ca95ae8d6d65a4d3bc80c06b11ed5f356be
andrewstech <45342431+andrewstech@users.noreply.github.com>
2021-06-07 17:51:44 +0100

    Fix syntax error

--- thealphavideo/__main__.py
+++ thealphavideo/__main__.py
@@ -39,7 +39,6 @@ def start():
         # of transactions for performance monitoring.
         # We recommend adjusting this value in production.
         traces_sample_rate=1.0
-        release="unofficial-skills@84a299379625"
     )
 
 
