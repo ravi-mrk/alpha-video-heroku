c00aa8ac97b8c40894f7195f63593e729d394a45
andrewstech <45342431+andrewstech@users.noreply.github.com>
2021-06-12 18:01:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     author_email=PROJECT_EMAIL,
     packages=PACKAGES,
     include_package_data=True,
-    package_data={'thealphavideo': ['templates/*.html']},
+    package_data={'*'},
     zip_safe=False,
     install_requires=requirements,
     test_suite="tests",
