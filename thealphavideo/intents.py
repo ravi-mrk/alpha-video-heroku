f0cf0be8f4a026f96d33ebd1753186e286beab8e
andrewstech <45342431+andrewstech@users.noreply.github.com>
2021-06-25 09:36:24 +0100

    Update intents.py

--- thealphavideo/intents.py
+++ thealphavideo/intents.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 from flask_ask_alphavideo import Ask, question, statement, audio, current_stream, logger, convert_errors
 from youtube_dl import YoutubeDL
 from pytube import *
-blueprint = Blueprint('blueprint_api', __name__, url_prefix="/ask")
+blueprint = Blueprint('blueprint_api', __name__, url_prefix="/api")
 ask = Ask(blueprint=blueprint)
 
 @ask.launch
