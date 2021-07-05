2351c885cb0acec51c931a3b2b2c44238680d8ec
andrewstech <45342431+andrewstech@users.noreply.github.com>
2021-07-05 15:12:40 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,7 +26,7 @@ module.exports = {
     algolia: {
       apiKey: 'd62d1c5918ea1492fbae354110c2cfed',
       indexName: 'andrewstech',
-      contextualSearch: false,
+      contextualSearch: true,
       searchParameters: { 'facetFilters': ["type:content"]}
       },
     navbar: {
